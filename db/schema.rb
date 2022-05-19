07f1dcb60127fb20b792bd645296113fed8de49b
louisekmm <louisekmm@gmail.com>
2022-05-19 14:39:45 -0400

    Resolving conflit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_05_19_161640) do
-=======
-ActiveRecord::Schema.define(version: 2022_05_19_160158) do
->>>>>>> parent of e3af9f0 (Add many to many warehouse-inventory_item)
 
   create_table "inventoryitems", force: :cascade do |t|
     t.date "expiration_date"
