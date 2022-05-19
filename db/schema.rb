ad4d55efe114dae9ce859d4363282998cc36ed30
louisekmm <louisekmm@gmail.com>
2022-05-19 14:29:43 -0400

    Revert "Add many to many warehouse-inventory_item"
    This reverts commit e3af9f0651a37aa391a4f5bd0017c7972d9941de.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_05_19_161640) do
+=======
+ActiveRecord::Schema.define(version: 2022_05_19_160158) do
+>>>>>>> parent of e3af9f0 (Add many to many warehouse-inventory_item)
 
   create_table "inventoryitems", force: :cascade do |t|
     t.date "expiration_date"
@@ -34,15 +38,6 @@ ActiveRecord::Schema.define(version: 2022_05_19_161640) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "warehouse_inventory_items", force: :cascade do |t|
-    t.integer "warehouse_id", null: false
-    t.integer "inventory_item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["inventory_item_id"], name: "index_warehouse_inventory_items_on_inventory_item_id"
-    t.index ["warehouse_id"], name: "index_warehouse_inventory_items_on_warehouse_id"
-  end
-
   create_table "warehouses", force: :cascade do |t|
     t.string "name"
     t.string "zip_code"
@@ -53,6 +48,4 @@ ActiveRecord::Schema.define(version: 2022_05_19_161640) do
   end
 
   add_foreign_key "inventoryitems", "products"
-  add_foreign_key "warehouse_inventory_items", "inventory_items"
-  add_foreign_key "warehouse_inventory_items", "warehouses"
 end
