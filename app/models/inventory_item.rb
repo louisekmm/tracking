40101ae4c39030eb344e93afbf2c33affbae454e
louisekmm <louisekmm@gmail.com>
2022-05-22 17:11:51 -0400

    Add inventory item validations

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -3,7 +3,8 @@ class InventoryItem < ApplicationRecord
   belongs_to :product
   belongs_to :warehouse
 
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: %w(Stock Reserved Incoming Sold),
+    message: "%{value} is not a valid status" }
   validates :barcode, presence: true
   validates :lot_number, presence: true
   validates :expiration_date, presence: true, format: { with: /\d{4}\-\d{2}\-\d{2}/, message: "must be yyyy-mm-dd" }
