338dfa80ddf6ba45211ffaf6b89e4b5a8a0bc56c
louisekmm <louisekmm@gmail.com>
2022-05-22 17:49:20 -0400

    Add dependent: destroy to warehouse model

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,6 +1,6 @@
 class Warehouse < ApplicationRecord
 
-  has_many :inventory_items
+  has_many :inventory_items, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :zip_code, presence: true, length: { minimum: 7, maximum:7 }, format: { with: /\A[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d\z/, message: "uses canadian format X0X 0X0 uppercase" }
