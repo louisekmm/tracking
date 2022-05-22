a97ff750936c02ed13b50ee05133d142e0df16b5
louisekmm <louisekmm@gmail.com>
2022-05-22 17:49:31 -0400

    Add tests for warehouses controller

--- test/controllers/warehouses_controller_test.rb
+++ test/controllers/warehouses_controller_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class WarehousesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @warehouse = Warehouse.first
+  end
+
+  test "POST warehouse#create creates a new warehouse" do
+    post "/warehouses", :params => {
+      :warehouse => {
+        :name => "Ottawa Branch",
+        :zip_code => "K1R 7T7",
+        :city => "Ottawa",
+        :province => "ON",
+      }
+    }
+
+    new_warehouse = assigns(:warehouse)
+    assert Warehouse.find(new_warehouse.id)
+  end
+
+  test "PUT warehouse#update edits a warehouse property" do
+    warehouse_id = @warehouse.id
+
+    put "/warehouses/#{warehouse_id}", :params => {
+      :warehouse => {
+        :name => "Ottawa Branch 2",
+      }
+    }
+
+    assert_equal Warehouse.find(warehouse_id).name, "Ottawa Branch 2"
+  end
+
+  test "DELETE warehouse#delete deletes a warehouse" do
+    warehouse_id = @warehouse.id
+
+    delete "/warehouses/#{warehouse_id}"
+
+    assert_nil Warehouse.find_by(id: warehouse_id)
+  end
 end
